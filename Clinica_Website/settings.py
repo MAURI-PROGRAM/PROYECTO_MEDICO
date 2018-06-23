40672cab9094df8f8f3d31613c6fa4416eaf96fc
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2018-06-23 12:12:57 -0500

    Configuracion de conexion a la base de datos

--- Clinica_Website/settings.py
+++ Clinica_Website/settings.py
@@ -73,11 +73,22 @@ WSGI_APPLICATION = 'Clinica_Website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'MANTENIMIENTO',
+         'USER': 'root',
+         'PASSWORD': '',
+         'HOST': '127.0.0.1',  # Or an IP Address that your DB is hosted on
+         'PORT': '3306',
+     }
 }
 
 
