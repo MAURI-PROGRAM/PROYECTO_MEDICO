224270c3124015202b85f35fb8cc1a82e7577306
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2018-06-23 12:57:27 -0500

    Configuracion agregar aplicacion

--- Clinica_Website/settings.py
+++ Clinica_Website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Registros.apps.RegistrosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
