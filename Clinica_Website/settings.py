49620e35c83dddd79ffe8a0d03d5710abcf89012
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2018-06-23 12:16:33 -0500

    Configuracion de conexion a la base de datos

--- Clinica_Website/settings.py
+++ Clinica_Website/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'Clinica_Website.wsgi.application'
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'MANTENIMIENTO',
+         'NAME': 'sistemamedico',
          'USER': 'root',
          'PASSWORD': '',
          'HOST': '127.0.0.1',  # Or an IP Address that your DB is hosted on
