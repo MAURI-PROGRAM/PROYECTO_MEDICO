8e1c461b27b4f74dad2d9aa49b92f72aacbd17db
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2018-07-05 13:48:33 -0500

    colocando namespace en la url

--- Registros/urls.py
+++ Registros/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name='registros'
+
 urlpatterns = [
 	path('', views.index,name='index'),
 	path('<int:id_p>', views.detail,name='detalles'),
