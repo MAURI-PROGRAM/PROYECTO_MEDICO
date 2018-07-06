fc5852fb5bb0f0b3f2e0a01cb71bf54f973f10ac
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2018-07-05 19:56:11 -0500

    creando modelo analisismamas

--- Registros/admin.py
+++ Registros/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Paciente,Diagnostico,farmacoterapia
+from .models import Paciente,Diagnostico,farmacoterapia,analisisMamas
 # Register your models here.
 
 admin.site.register(Paciente)
 admin.site.register(Diagnostico)
 admin.site.register(farmacoterapia)
+admin.site.register(analisisMamas)
