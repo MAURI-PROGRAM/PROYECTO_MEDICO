9dfef3dc814ee92503af58f9f7be24da15378225
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2018-07-06 19:39:01 -0500

    creando modelo analisisAbdomen

--- Registros/admin.py
+++ Registros/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Paciente,Diagnostico,farmacoterapia,analisisMamas
+from .models import Paciente,Diagnostico,farmacoterapia,analisisMamas,analisisAbdominal
 # Register your models here.
 
 admin.site.register(Paciente)
 admin.site.register(Diagnostico)
 admin.site.register(farmacoterapia)
 admin.site.register(analisisMamas)
+admin.site.register(analisisAbdominal)
