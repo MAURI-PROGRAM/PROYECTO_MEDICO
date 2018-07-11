6446e588319f2e57fd77c422920ece43400c9f93
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2018-07-10 19:26:15 -0500

    creando modelo analisisObstetrico

--- Registros/admin.py
+++ Registros/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Paciente,Diagnostico,farmacoterapia,analisisMamas,analisisAbdominal
+from .models import Paciente,Diagnostico,farmacoterapia,analisisMamas,analisisAbdominal,analisisObstetrico
 # Register your models here.
 
 admin.site.register(Paciente)
@@ -7,3 +7,4 @@ admin.site.register(Diagnostico)
 admin.site.register(farmacoterapia)
 admin.site.register(analisisMamas)
 admin.site.register(analisisAbdominal)
+admin.site.register(analisisObstetrico)
