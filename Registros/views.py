2b425960b3176525b72a75646461df27583bd2af
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-01-16 12:16:51 -0500

    Eliminando redundancia en formularios ecografias

--- Registros/views.py
+++ Registros/views.py
@@ -76,68 +76,78 @@ def get_initial_pri(self, **kwargs):
     paciente = Paciente.objects.get(pk=parametro)
     return {'paciente':paciente,}
 
-def get_context_data_pri(self, nomclass,**kwargs):
+def get_context_data_pri(self, nomclass,extra,**kwargs):
     parametro = self.kwargs.get('ident',None)
     paciente = Paciente.objects.get(pk=parametro)
     context = super(nomclass, self).get_context_data(**kwargs) 
     context['paciente']= paciente
+    context['extrainfo'] = extra
     print(context)
     return context
 
 class EcomamaCreate(CreateView):
     model = analisisMamas
     fields='__all__'
-    template_name='registros/ecomamanew.html'
+    template_name='registros/base_ecografias_form.html'
     context_object_name='datos'
     def get_initial(self, **kwargs):
         return get_initial_pri(self, **kwargs)
     def get_context_data(self, **kwargs):
-        return get_context_data_pri(self,EcomamaCreate, **kwargs)
+        extrainfo={'title':'Ecografia Mamas','page':'Ecografia Mamas'}
+        return get_context_data_pri(self,EcomamaCreate,extrainfo, **kwargs)
 
 class EcoabdomenCreate(CreateView):
     model=analisisAbdominal
     fields='__all__'
-    template_name='registros/ecoabdomennew.html'
+    template_name='registros/base_ecografias_form.html'
     def get_initial(self, **kwargs):
         return get_initial_pri(self, **kwargs)
     def get_context_data(self, **kwargs):
-        return get_context_data_pri(self,EcoabdomenCreate, **kwargs)
+        extrainfo={'title':'Ecografia abdominal','page':'Ecografia abdominal'}
+        return get_context_data_pri(self,EcoabdomenCreate,extrainfo, **kwargs)
 
 class EcoobstetricoCreate(CreateView):
     model=analisisObstetrico
     fields='__all__'
-    template_name='registros/ecoobstericonew.html'
+    template_name='registros/base_ecografias_form.html'
     def get_initial(self, **kwargs):
         return get_initial_pri(self, **kwargs)
     def get_context_data(self, **kwargs):
-        return get_context_data_pri(self,EcoobstetricoCreate, **kwargs)
+        extrainfo={'title':'Ecografia Obstetrico','page':'Ecografia Obstetrico'}
+        return get_context_data_pri(self,EcoobstetricoCreate,extrainfo, **kwargs)
 
 class EcorenalCreate(CreateView):
     model=ecografiaRenal
     fields='__all__'
-    template_name='registros/ecorenalnew.html'
+    template_name='registros/base_ecografias_form.html'
     def get_initial(self, **kwargs):
         return get_initial_pri(self, **kwargs)
     def get_context_data(self, **kwargs):
-        return get_context_data_pri(self,EcorenalCreate, **kwargs)
+        extrainfo={'title':'Ecografia Renal','page':'Ecografia Renal'}
+        return get_context_data_pri(self,EcorenalCreate,extrainfo, **kwargs)
 
 class EcoginecologiaCreate(CreateView):
     model=ecografiaginecologico
     fields='__all__'
-    template_name='registros/ecoginecologianew.html'
+    template_name='registros/base_ecografias_form.html'
     def get_initial(self, **kwargs):
         return get_initial_pri(self, **kwargs)
     def get_context_data(self, **kwargs):
-        return get_context_data_pri(self,EcoginecologiaCreate, **kwargs)
+        extrainfo={'title':'Ecografia Ginecologia','page':'Ecografia Ginecologia'}
+        return get_context_data_pri(self,EcoginecologiaCreate,extrainfo, **kwargs)
 
 class EcotesticularCreate(CreateView):
     model=ecografiatesticular
     fields='__all__'
-    template_name='registros/ecotesticularnew.html'
+    template_name='registros/base_ecografias_form.html'
     def get_initial(self, **kwargs):
         return get_initial_pri(self, **kwargs)
     def get_context_data(self, **kwargs):
-        return get_context_data_pri(self,EcotesticularCreate, **kwargs)
+        extrainfo={'title':'Ecografia Testicular','page':'Ecografia Testicular'}
+        return get_context_data_pri(self,EcotesticularCreate,extrainfo, **kwargs)
+
+
+
 
 class ListDiagnostico(DetailView):
     model = Paciente
